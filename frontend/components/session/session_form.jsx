0577058df9f4bd914b53fada37f88578fc7c27d6
mohammad <m.a.taghva@gmail.com>
2021-06-09 15:42:23 -0700

    Change background image url

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -97,7 +97,7 @@ class SessionForm extends React.Component {
     render() {
         return (
             <div>
-                <img className="session_image" src="/assets/home_image3.jpg" alt="Trail" />
+                <img className="session_image" src="https://footprint-pro.s3.us-west-1.amazonaws.com/home_image3.jpg" alt="Trail" />
                 <div className="session_container">
                     <div className="session_form_title">{this.props.title}</div>
                     <form onSubmit={this.handleSubmit} className="session_form">
